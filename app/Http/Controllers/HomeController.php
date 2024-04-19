b02a8b8f8bb5a5bc57214385513865a5fb489488
Mr Nim <mr.nim94@gmail.com>
2024-04-19 17:41:20 +0000

    update save guest_confirmations

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,4 +9,17 @@ class HomeController extends Controller
     public function index(){
         return view('home');
     }
+
+    public function save_guest_confirmations(Request $request){
+        $data = array();
+        $data['your_name'] = $request->your_name;
+        $data['attendance_status'] = $request->attendance_status;
+        $data['relationship_to_couple'] = $request->relationship_to_couple;
+        $data['party_size'] = $request->party_size;
+        $data['message_to_couple'] = $request->message_to_couple;
+        echo '<pre>';
+        print_r($data);
+        echo '</pre>';
+
+    }
 }
