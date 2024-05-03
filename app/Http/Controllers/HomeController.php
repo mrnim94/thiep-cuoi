6d3248456c22bb335a27e6108ee308a41c1c2c16
Mr Nim <mr.nim94@gmail.com>
2024-05-03 08:21:18 +0000

    fix time

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -7,7 +7,7 @@ use DB;
 use Session;
 use Illuminate\Support\Facades\Redirect;
 use App\Http\Requests;
-use Carbon\Carbon;
+
 session_start();
 
 class HomeController extends Controller
@@ -25,7 +25,7 @@ class HomeController extends Controller
         $data['message_to_couple'] = $request->message_to_couple;
 
         // Set timestamps to UTC+7
-        $currentTimestamp = Carbon::now('UTC+7');
+        $currentTimestamp = now('Europe/London')->toIso8601String();
         $data['created_at'] = $currentTimestamp;
         $data['updated_at'] = $currentTimestamp;
         
